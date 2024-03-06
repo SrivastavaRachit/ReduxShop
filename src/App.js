ca24a4b2840274eca52e4c5a00c79a3229c7d727
SrivastavaRachit <rachitsrivastava@gmail.com>
2024-03-06 16:54:40 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
